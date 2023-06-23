5b58aaeef9a8ebf909e2ad88187f42192168b397
iangalvao <iansgalvao@gmail.com>
2023-06-23 18:27:00 -0300

    tests update

--- tests/test_map_class.py
+++ tests/test_map_class.py
@@ -1,123 +1,50 @@
 import pytest
-from app.app import GameMap,Tile
-""" 
+from app.app import GameMap, Tile
+import pytest
 
-class GameMapTestCase(unittest.TestCase):
-    def setUp(self):
-        self.game_map = GameMap(5)
-        
-    def test_distance_calculation(self):
-        # Test distance calculation
-        distance = self.game_map.point_distance((0, 0), (3, 4))
-        self.assertAlmostEqual(distance, 5.0)
-    
-    def test_get_neighbours_within_radius(self):
 
-        # Call the get_neighbours method with a radius of 1
-        neighbors = self.game_map.get_neighbours((2, 2), 1)
-        
-        # Assert that the center tile is not in the neighbors list
-        self.assertNotIn(self.game_map.tiles[2][2], neighbors)
-        
-        # Assert that all neighboring tiles are in the neighbors list
-        expected_neighbors = [
-            self.game_map.tiles[1][2],
-            self.game_map.tiles[3][2],
-            self.game_map.tiles[2][1],
-            self.game_map.tiles[2][3]
-        ]
-        for neighbor in expected_neighbors:
-            self.assertIn(neighbor, neighbors)
-    
-    def test_out_of_bounds(self):
-        # Test out_of_bounds method with valid and invalid coordinates
-        self.assertTrue(self.game_map.out_of_bounds((-1, 0)))
-        self.assertFalse(self.game_map.out_of_bounds((0, 0)))
-        self.assertFalse(self.game_map.out_of_bounds((4, 4)))
-        self.assertTrue(self.game_map.out_of_bounds((5, 5)))
+@pytest.fixture
+def game_map():
+    return GameMap(5)
 
- """
-def test_get_neighbours_in_radius_1():
-    # Create a game map of size 5x5
-    game_map = GameMap(5)
-    
-    # Set a specific tile as the center tile
-    center_tile = game_map.tiles[2][2] 
-    
-    # Call the get_neighbours method with a radius of 1
-    neighbors = game_map.get_neighbours((2, 2), 1)
-    
-    # Assert that the center tile is not in the neighbors list
-    assert center_tile not in neighbors
-    
-    # Assert that all neighboring tiles are in the neighbors list
-    assert game_map.tiles[1][2] in neighbors
-    assert game_map.tiles[3][2] in neighbors
-    assert game_map.tiles[2][1] in neighbors
-    assert game_map.tiles[2][3] in neighbors
-    
+
+@pytest.mark.parametrize(
+    "point_a, point_b, expected_result",
+    [
+        ((1, 1), (1, 2), 1),
+        ((1, 1), (1, 1), 0),
+        ((1, 1), (2, 1), 1),
+        ((2, 1), (1, 1), 1),
+        ((1, 2), (1, 1), 1),
+        ((1, 1), (1, 2), 1),
+        ((0, 0), (3, 3), 4.5),
+        ((0, 0), (0, 4), 4),
+        ((4, 4), (4, 0), 4),
+        ((1, 1), (2, 2), 1.5),
+    ],
+)
+def test_game_map_distance(game_map, point_a, point_b, expected_result):
+    assert game_map.point_distance(point_a, point_b) == expected_result
+    print("point_distance test passed!")
+
+
+@pytest.mark.parametrize(
+    "center, radius, expected_result",
+    [
+        ((1, 1), 1, ((0, 1), (1, 0), (2, 1), (1, 2))),
+        ((1, 1), 1.5, ((2, 2), (0, 0), (0, 2), (2, 0))),
+        ((0, 0), 1.5, ((0, 1), (1, 0), (1, 1))),
+        ((1, 0), 1, ((0, 0), (2, 0), (1, 1))),
+    ],
+)
+def test_get_neighbours_valid_inputs(game_map, center, radius, expected_result):
+    neighbors = game_map.get_neighbours(center, radius)
+    assert game_map.tiles[center[0]][center[1]] not in neighbors
+    for pos in expected_result:
+        x, y = pos
+        assert game_map.tiles[x][y] in neighbors
     print("get_neighbours test passed!")
 
 
-
-def test_get_neighbours_in_radius_1_and_a_half():
-    # Create a game map of size 5x5
-    game_map = GameMap(5)
-    
-    # Set a specific tile as the center tile
-    center_tile = game_map.tiles[2][2] 
-    
-    # Call the get_neighbours method with a radius of 1.5
-    neighbors = game_map.get_neighbours((2, 2), 1.5)
-
-    # Assert that all neighboring tiles are in the neighbors list
-    assert game_map.tiles[3][3] in neighbors
-    assert game_map.tiles[1][3] in neighbors
-    assert game_map.tiles[3][1] in neighbors
-    assert game_map.tiles[1][1] in neighbors
-
-    # Assert that all tiles in radius 2 are not in the neighbors list
-    assert game_map.tiles[4][2] not in neighbors
-    assert game_map.tiles[0][2] not in neighbors
-    assert game_map.tiles[2][4] not in neighbors
-    assert game_map.tiles[2][0] not in neighbors
-
-    print("get_neighbours test passed!")
-
-def test_get_neighbours_in_radius_1_and_a_half_on_0_0():
-    # Create a game map of size 5x5
-    game_map = GameMap(5)
-    
-    # Set a specific tile as the center tile
-    center_pos = (0,0)
-    
-    # Call the get_neighbours method with a radius of 1.5
-    neighbors = game_map.get_neighbours((center_pos), 1.5)
-
-    # Assert that all neighboring tiles are in the neighbors list
-    assert game_map.tiles[0][1] in neighbors
-    assert game_map.tiles[1][0] in neighbors
-    assert game_map.tiles[1][1] in neighbors
-
-    assert len(neighbors) == 3
-    print("get_neighbours test passed!")
-
-def test_get_neighbours_in_radius_1_in_map_edge_should_have_3_neighbours():
-    # Create a game map of size 5x5
-    game_map = GameMap(5)
-    
-    # Set a specific tile as the center tile
-    center_pos = (1,0)
-    
-    # Call the get_neighbours method with a radius of 1
-    neighbors = game_map.get_neighbours((center_pos), 1)
-
-    # Assert that all neighboring tiles are in the neighbors list
-    assert game_map.tiles[0][0] in neighbors
-    assert game_map.tiles[2][0] in neighbors
-    assert game_map.tiles[1][1] in neighbors
-
-    assert len(neighbors) == 3
-    print("get_neighbours test passed!")
 # Run the test
 pytest.main()
