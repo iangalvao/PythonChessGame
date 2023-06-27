77e295a0d5890d11f4cec770afb3f4bae73dd940
iangalvao <iansgalvao@gmail.com>
2023-06-27 17:53:34 -0300

    entities

---
+++ app/entities/game.py
@@ -0,0 +1,4 @@
+class Game:
+    def __init__(self, players) -> None:
+        self.turn = 0
+        self.players = players
---
+++ app/entities/unit.py
@@ -0,0 +1,5 @@
+class Unit():
+    def __init__(self, type) -> None:
+        self.type = type
+        self.pos = None
+    
