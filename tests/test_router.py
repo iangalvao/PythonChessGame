7a6d5cc0c821395c122f219b5435c5e91743dafe
iangalvao <iansgalvao@gmail.com>
2023-08-13 16:53:24 -0300

    router unity tests

---
+++ tests/test_router.py
@@ -0,0 +1,27 @@
+import pytest
+from unittest.mock import MagicMock, patch
+from app.router.router import Router
+from app.controllers.commandHandlerInterface import CommandHandlerInterface
+
+
+@pytest.fixture
+def router():
+    commandHandler = MagicMock
+    return Router(commandHandler())
+
+
+def test_call_to_execute_with_valid_inputs(router):
+    router.execute(["walk", "63", "6"])
+    router.controller.walk.assert_called_once_with(["63", "6"])
+
+
+def test_execute_quit_command_should_exit(router):
+    # SETUP
+    request = ["quit"]
+
+    # Mocking exit function
+    with patch("builtins.exit") as mocker_exit:
+        # ACTION
+        router.execute(request)
+        # ASSERTS
+        mocker_exit.assert_called_once_with(0)
