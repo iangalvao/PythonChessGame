632b0bc5ec032e6952bb6d07f14d94a37efc96fc
iangalvao <iansgalvao@gmail.com>
2023-08-24 11:16:51 -0300

    refactor matchhandler tests

--- tests/test_match_handler.py
+++ tests/test_match_handler.py
@@ -16,14 +16,17 @@ def match_handler() -> MatchHandler:
 @patch("app.entities.match.Match.next_turn", return_value=None)
 @patch("app.entities.match.Match.get_turn", return_value=1)
 def test_call_to_next_turn_should_call_match_next_turn(
-    get_turn_mocker, next_turn_mocker, match_handler: MatchHandler
+    match_get_turn_mocker, match_next_turn_mocker, match_handler: MatchHandler
 ):
     # Mock the helper_method to return a specific value
     with patch.object(
         match_handler, "start_turn", return_value=None
-    ) as start_turn_mocker:
+    ) as match_handler_start_turn_mocker:
+        #ACTION
         match_handler.next_turn()
-        next_turn_mocker.assert_called_once_with()
-        get_turn_mocker.assert_called_once_with()
-        start_turn_mocker.assert_called_once_with(match_handler.match.players[0])
-        match_handler.presenter.next_turn.assert_called_once_with(1)
+        # MatchHandler object's ASSERTS
+        match_handler_start_turn_mocker.assert_called_once_with(match_handler.match.players[0]) # called with next player
+        match_handler.presenter.next_turn.assert_called_once_with(1) # called with turn 1 (next turn)
+        # children Match object's ASSERTS
+        match_next_turn_mocker.assert_called_once_with()
+        match_get_turn_mocker.assert_called_once_with()
